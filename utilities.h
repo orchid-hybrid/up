41d69216d1186febf79d20cf47957452dbf849b3
name <orchid-hybrid@users.noreply.github.com>
2015-01-31 02:43:58 +0000

    Create utilities.h

---
+++ utilities.h
@@ -0,0 +1,9 @@
+
+// write to file returns 0 on success, -1 on failure
+// it will overwrite a file if it already exists
+int write_to_file(char *filename, unsigned char bytes[], size_t bytes_length);
+
+// read from file returns 0 on success, -1 on failure
+// it allocates a buffer in 'bytes' which you are expected to free
+// the size of the file is written into 'bytes_length'
+int read_from_file(char *filename, unsigned char **bytes, size_t *bytes_length);
