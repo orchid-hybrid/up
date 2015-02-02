d6741698c1ef37127bf3fba5ab3401921eaa450d
name <orchid-hybrid@users.noreply.github.com>
2015-02-02 16:23:32 +0000

    Update t7.c

--- t7.c
+++ t7.c
@@ -12,6 +12,27 @@
 #include "utilities.h"
 #include "conf.h"
 
+// This program lets you test making a TCP connection from one computer to another
+// you can also just make a connection from your own computer to itself.
+// to do that pick a four digit port number >1024
+//
+// run ./t7 listen 127.0.0.1 <PORT>
+// and in another one ./t7 send 127.0.0.1 <PORT>
+//
+// if this works you could test it from one computer to another.
+// to do that the listener/server side will have to tell its router to forward
+// a port to your local ip. It might be listed in `ip addr` or some other command
+// you will also need to find the external IP of the listener, to do that
+// google: what is my ip
+//
+// run ./t7 listen <local-ip> <PORT>
+// on the other computer run ./t7 send <external-ip> <PORT>
+//
+// useful root commands to debug this sort of thing on linux:
+// * lsof -i
+// * netstat -lptu
+// * netstat -tulpn
+
 #define send_mode 0
 #define listen_mode 1
 
