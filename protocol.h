7816656ed1d1786608ffc9dfd32b694901423f09
yuuko <yuuko@users.noreply.github.com>
2015-02-04 21:50:25 +0000

    added some notes to protocol.h regarding argument sizes

--- protocol.h
+++ protocol.h
@@ -1,6 +1,7 @@
-int key_exchange(unsigned char *a_pk,
-                 unsigned char *a_sk,
-                 unsigned char *b_pk,
-                 unsigned char *key,
+// sizes of arguments are as follows:
+int key_exchange(unsigned char *a_pk,   // crypto_box_PUBLICKEYBYTES
+                 unsigned char *a_sk,   // crypto_box_SECRETKEYBYTES
+                 unsigned char *b_pk,   // crypto_box_PUBLICKEYBYTES
+                 unsigned char *key,    // padded, crypto_box_ZEROBYTES + crypto_secretbox_KEYBYTES
                  int mode,
                  int sock);
