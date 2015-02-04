f7ba7574b3e49de67d58106a8c77e77a92a3b7ce
orchid.hybrid <orchid.hybrid@gmail.com>
2015-02-04 01:16:40 +0000

    working towards fixing what i broke..

--- t8.c
+++ t8.c
@@ -116,14 +116,14 @@ int main(int argc, char **argv) {
   
   b_epk = padded_array_make(b_epk_bytes, crypto_box_ZEROBYTES, crypto_box_PUBLICKEYBYTES);
   
-#define FAIL(er) { fprintf(stderr, "%s failure", er); close(sock); return EXIT_FAILURE; }
+#define FAIL(er) { fprintf(stderr, "%s failure\n", er); close(sock); return EXIT_FAILURE; }
   
   // 1. Alice and Bob generate new ephemeral keypairs
   crypto_box_keypair(a_epk.start, a_esk.start);
   
   // 2a. Alice encrypts her ephemeral public key with her non-ephemeral private key and Bob's non-ephemeral public key
   // 2b. Bob encrypts his ephemeral public key with his non-ephemeral private key and Alice's non-ephemeral public key
-  crypto_box(a_epk_enc.bytes, a_epk.bytes, a_epk.padded_length, n, b_pk, a_sk);
+  if(crypto_box(a_epk_enc.bytes, a_epk.bytes, a_epk.padded_length, n, b_pk, a_sk)) { FAIL("crypto_box 1"); }
 
   // 3. Alice and Bob send each other their encrypted ephemeral public keys
   if(mode == send_mode) {
@@ -131,17 +131,17 @@ int main(int argc, char **argv) {
     sendall(sock, a_epk_enc.start, a_epk_enc.length);
     
     // next, we recieve Bob's ephemeral public key,
-    if (recv(sock, b_epk_enc.start, b_epk_enc.length, MSG_WAITALL) != b_epk_enc.length) { FAIL("recv") }
+    if (recv(sock, b_epk_enc.start, b_epk_enc.length, MSG_WAITALL) != b_epk_enc.length) { FAIL("recv 2") }
   }
   else if(mode == listen_mode) { 
     // daemon mode is identical to client mode except inverted send/recv
-    if (recv(sock, b_epk_enc.start, b_epk_enc.length, MSG_WAITALL) != b_epk_enc.length) { FAIL("recv") }
+    if (recv(sock, b_epk_enc.start, b_epk_enc.length, MSG_WAITALL) != b_epk_enc.length) { FAIL("recv 3") }
     
     sendall(sock, a_epk_enc.start, a_epk_enc.length);
   }
 
   // and decrypt it using Bob's non-ephemeral public key, and our non-ephemeral secret key
-  if (crypto_box_open(b_epk.bytes, b_epk_enc.bytes, b_epk_enc.padded_length, n, b_pk, a_sk)) { FAIL("crypto_box_open") }
+  if(crypto_box_open(b_epk.bytes, b_epk_enc.bytes, b_epk_enc.padded_length, n, b_pk, a_sk)) { FAIL("crypto_box_open 4") }
   
   unsigned char a_key_bytes[crypto_box_ZEROBYTES + crypto_secretbox_KEYBYTES];
   unsigned char a_key_enc_bytes[crypto_secretbox_KEYBYTES + crypto_box_ZEROBYTES];
@@ -165,20 +165,20 @@ int main(int argc, char **argv) {
   
   // 5a. Alice encrypts her half of the symmetric key with her ephemeral private key and Bob's ephemeral public key, and sends this to him
   // 5b. Bob encrypts his half of the symmetric key with his ephemeral private key and Alice's ephemeral public key, and sends this to her
-  crypto_box(a_key_enc.bytes, a_key.bytes, a_key.padded_length, n, b_epk.start, a_esk.start);
+  if(crypto_box(a_key_enc.bytes, a_key.bytes, a_key.padded_length, n, b_epk.start, a_esk.start)) { FAIL("crypto_box 5") }
   if(mode == send_mode) {
     sendall(sock, a_key_enc.start, a_key_enc.length);
-    if (recv(sock, b_key_enc.start, b_key_enc.length, MSG_WAITALL) != b_key_enc.length) { FAIL("recv") }
+    if (recv(sock, b_key_enc.start, b_key_enc.length, MSG_WAITALL) != b_key_enc.length) { FAIL("recv 6") }
   }
   else if(mode == listen_mode) {
-    if (recv(sock, b_key_enc.start, b_key_enc.length, MSG_WAITALL) != b_key_enc.length) { FAIL("recv") }
+    if (recv(sock, b_key_enc.start, b_key_enc.length, MSG_WAITALL) != b_key_enc.length) { FAIL("recv 7") }
     sendall(sock, a_key_enc.start, a_key_enc.length);
   }
   memset(a_key_enc.start, 0x00, a_key_enc.length);
   
   // 6. Alice and Bob decrypt their respective halves of the symmetric key
   // now we decrypt Bob's symmetric key using his ephemeral public key and our ephemeral private key
-  crypto_box_open(b_key.bytes, b_key_enc.bytes, b_key_enc.padded_length, n, b_epk.start, a_esk.start);
+  if(crypto_box_open(b_key.bytes, b_key_enc.bytes, b_key_enc.padded_length, n, b_epk.start, a_esk.start)) { FAIL("crypto_box_open 8") }
   
   int i;
   
